a0d2758986b8f9c1ee4c1c9e95c34b40b2d09370
Shanur Rehman <rahman.shanur7@gmail.com>
2017-04-24 04:25:10 +0530

    This will be the last commit of the night
    Nothing much since last commit

--- api/restapi/models.py
+++ api/restapi/models.py
@@ -2,11 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
+#id is being automatically generated therefore there is no need for it
+#we can retrive that data from that id itself
 class Names(models.Model):
-    student_id=models.IntegerField()
     names=models.CharField(max_length=100)
-    roll=models.CharField(max_length=10)
+#Deleting roll number also since it is not required. Tring to be professional
+#Anyways
 
     def __str__(self):
-        return self.names + '     '+self.roll
+        return self.names
